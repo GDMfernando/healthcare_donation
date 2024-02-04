fcb04c932fc85df5e44453f9e4ced7531cc8ff61
Dasni Fernando <Dasnif@opusxenta.com>
2024-02-04 19:54:53 +0530

    changes

--- frontend/src/components/admin_view/admin_nav.js
+++ frontend/src/components/admin_view/admin_nav.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Container, Nav, NavDropdown, Offcanvas, Form, Button } from 'react-bootstrap';
+import { Navbar, Container, Nav, NavDropdown, Offcanvas } from 'react-bootstrap';
 
 function AdminNav() {
     return (

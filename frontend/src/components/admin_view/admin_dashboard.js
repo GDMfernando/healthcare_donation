463e078c0b7713b050a63444969454a7a6443770
Dasni Fernando <Dasnif@opusxenta.com>
2024-02-14 20:59:27 +0530

    get all hospitla data to froontend

--- frontend/src/components/admin_view/admin_dashboard.js
+++ frontend/src/components/admin_view/admin_dashboard.js
@@ -1,54 +1,78 @@
-import React from 'react';
-import Col from 'react-bootstrap/Col';
-import Nav from 'react-bootstrap/Nav';
-import Row from 'react-bootstrap/Row';
-import Tab from 'react-bootstrap/Tab';
-import AdminNav from './admin_nav';
-import MainDashboard from './main_dashboard';
-import HospitalRegistration from './hospital_registration';
-import ManageHospitals from './manage_hospitals'
-import RegisterCampaigne from './register_campaign';
-import ManageCampaign from './manage_campaigne';
-import ViewDonations from './view_donation';
+import React, { useState } from "react";
+import Col from "react-bootstrap/Col";
+import Nav from "react-bootstrap/Nav";
+import Row from "react-bootstrap/Row";
+import Tab from "react-bootstrap/Tab";
+import AdminNav from "./admin_nav";
+import MainDashboard from "./main_dashboard";
+import HospitalRegistration from "./hospital_registration";
+import ManageHospitals from "./manage_hospitals";
+import RegisterCampaigne from "./register_campaign";
+import ManageCampaign from "./manage_campaigne";
+import ViewDonations from "./view_donation";
 
-function AdminDashboard(){
-    return (
-        <div>
-        <AdminNav/>
-        <Tab.Container id="left-tabs-example" defaultActiveKey="0">
-              <Row>
-                <Col sm={3}>
-                  <Nav variant="pills" className="flex-column">
-                    <Nav.Item>
-                      <Nav.Link eventKey="1">Register Hospital</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="2">Manage Hospitals</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="3">Register Campaign</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="4">Manage Campaigns</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="5">View Donations</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                </Col>
-                <Col sm={9}>
-                  <Tab.Content>
-                  <Tab.Pane eventKey="0"><MainDashboard/></Tab.Pane>
-                    <Tab.Pane eventKey="1"><HospitalRegistration /></Tab.Pane>
-                    <Tab.Pane eventKey="2"><ManageHospitals/></Tab.Pane>
-                    <Tab.Pane eventKey="3"><RegisterCampaigne/></Tab.Pane>
-                    <Tab.Pane eventKey="4"><ManageCampaign/></Tab.Pane>
-                    <Tab.Pane eventKey="5"><ViewDonations/></Tab.Pane>
-                  </Tab.Content>
-                </Col>
-              </Row>
-            </Tab.Container>
-            </div>
-      );
+function AdminDashboard() {
+  const [activeTab, setActiveTab] = useState("0"); // State to keep track of active tab
+
+  const handleTabSelect = (eventKey) => {
+    // console.log("Selected tab:", eventKey);
+    setActiveTab(eventKey);
+  };
+
+  return (
+    <div>
+      <AdminNav />
+      <Tab.Container
+        id="left-tabs-example"
+        defaultActiveKey="0"
+        activeKey={activeTab}
+        onSelect={handleTabSelect}
+      >
+        <Row>
+          <Col sm={3}>
+            <Nav variant="pills" className="flex-column">
+              <Nav.Item>
+                <Nav.Link eventKey="1">Register Hospital</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="2">Manage Hospitals</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="3">Register Campaign</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="4">Manage Campaigns</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="5">View Donations</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+          <Col sm={9}>
+            <Tab.Content>
+              <Tab.Pane eventKey="0">
+                <MainDashboard />
+              </Tab.Pane>
+              <Tab.Pane eventKey="1">
+                <HospitalRegistration />
+              </Tab.Pane>
+              <Tab.Pane eventKey="2">
+                <ManageHospitals activeTab={activeTab} />
+              </Tab.Pane>
+              <Tab.Pane eventKey="3">
+                <RegisterCampaigne />
+              </Tab.Pane>
+              <Tab.Pane eventKey="4">
+                <ManageCampaign />
+              </Tab.Pane>
+              <Tab.Pane eventKey="5">
+                <ViewDonations />
+              </Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
+    </div>
+  );
 }
 export default AdminDashboard;
--- frontend/src/components/admin_view/manage_hospitals.js
+++ frontend/src/components/admin_view/manage_hospitals.js
@@ -1,83 +1,109 @@
-import React, { useEffect, useState } from 'react';
-import { Table, Button, Form, Row, Col } from 'react-bootstrap';
-
-function ManageHospitals() {
-    const [hospitals, setHospitals] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('');
-
-    useEffect(() => {
-
-        fetch('http://localhost:3000/admin-dashboard')
-            .then(response => response.json())
-            .then(data => setHospitals(data))
-            .catch(error => console.error('Error:', error));
-    }, []);
-
-    const handleDelete = (id) => {
-
-        console.log(`Delete hospital with id: ${id}`);
+import React, { useEffect, useState } from "react";
+import { Table, Button, Form, Row, Col } from "react-bootstrap";
+import { useCookies } from "react-cookie";
+import { callAPI } from "../../utils/help";
+function ManageHospitals(activeTab = null) {
+  const [hospitals, setHospitals] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [cookie, _] = useCookies(["access_token"]);
+  useEffect(() => {
+    const getAll = async () => {
+      const fetchOptions = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${cookie.access_token}`,
+        },
+        withCredentials: true,
+      };
+      const response = await callAPI("admin/all", "GET", fetchOptions);
+      if (response.ok) {
+        const data = await response.json();
+        setHospitals(data.results);
+      } else {
+        console.log("Unauthorize");
+      }
     };
 
-    const handleEdit = (id) => {
+    getAll();
+    return () => {};
+  }, [activeTab]);
 
-        console.log(`Edit hospital with id: ${id}`);
-    };
+  const handleDelete = (id) => {
+    console.log(`Delete hospital with id: ${id}`);
+  };
 
-    const handleSearch = (e) => {
-        setSearchTerm(e.target.value);
-    };
+  const handleEdit = (id) => {
+    console.log(`Edit hospital with id: ${id}`);
+  };
 
-    const filteredHospitals = hospitals.filter((hospital) => {
-        return (
-            hospital.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-            hospital.address.toLowerCase().includes(searchTerm.toLowerCase()) ||
-            hospital.type.toLowerCase().includes(searchTerm.toLowerCase())
-        );
-    });
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
+  const filteredHospitals = hospitals.filter((hospital) => {
     return (
-        <div>
-            <Row>
-              <Col><h2>Manage Hospitals</h2></Col>  
-                <Form as={Col} className="mb-3">
-                    <Form.Group controlId="formSearch">
-                        <Form.Control type="text" placeholder="Search by name, address, or type" value={searchTerm} onChange={handleSearch} />
-                    </Form.Group>
-                </Form>
-            </Row>
+      hospital.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      hospital.address.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      hospital.type.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  });
 
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Address</th>
-                        <th>Phone</th>
-                        <th>Email</th>
-                        <th>Username</th>
-                        <th>Password</th>
-                        <th>Type</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {hospitals.map(hospital => (
-                        <tr key={hospital.id}>
-                            <td>{hospital.name}</td>
-                            <td>{hospital.address}</td>
-                            <td>{hospital.phone}</td>
-                            <td>{hospital.email}</td>
-                            <td>{hospital.username}</td>
-                            <td>{hospital.password}</td>
-                            <td>{hospital.type}</td>
-                            <td>
-                                <Button variant="primary" onClick={() => handleEdit(hospital.id)}>Edit</Button>
-                                <Button variant="danger" onClick={() => handleDelete(hospital.id)}>Delete</Button>{' '}
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </Table>
-        </div>
-    )
+  return (
+    <div>
+      <Row>
+        <Col>
+          <h2>Manage Hospitals</h2>
+        </Col>
+        <Form as={Col} className="mb-3">
+          <Form.Group controlId="formSearch">
+            <Form.Control
+              type="text"
+              placeholder="Search by name, address, or type"
+              value={searchTerm}
+              onChange={handleSearch}
+            />
+          </Form.Group>
+        </Form>
+      </Row>
+
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Phone</th>
+            <th>Email</th>
+            <th>Type</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {hospitals.map((hospital) => (
+            <tr key={hospital.id}>
+              <td>{hospital.name}</td>
+              <td>{hospital.address}</td>
+              <td>{hospital.phone_number}</td>
+              <td>{hospital.email}</td>
+              <td>{hospital.type}</td>
+              <td>
+                <Button
+                  variant="primary"
+                  onClick={() => handleEdit(hospital.id)}
+                >
+                  Edit
+                </Button>
+                <Button
+                  variant="danger"
+                  onClick={() => handleDelete(hospital.id)}
+                >
+                  Delete
+                </Button>{" "}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
 }
 export default ManageHospitals;

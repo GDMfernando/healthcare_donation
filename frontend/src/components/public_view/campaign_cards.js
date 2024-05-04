32e8f0356144c20beed320cb37dccc4d094579ef
Dasni Fernando <dasnifernando12@gmail.com>
2024-05-04 19:38:38 +0530

    adding prograssbar

--- frontend/src/components/public_view/campaign_cards.js
+++ frontend/src/components/public_view/campaign_cards.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Container, Card, Row, Col, Button } from "react-bootstrap";
+import {
+  Container,
+  Card,
+  Row,
+  Col,
+  Button,
+} from "react-bootstrap";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 function CampaignCards({ title, campaigns, onDonateButtonClick }) {
   const handleDonateCampaignButton = (campaignId) => {
@@ -12,29 +20,36 @@ function CampaignCards({ title, campaigns, onDonateButtonClick }) {
         <h2>{title}</h2>
       </div>
       <Row xs={2} md={4} className="g-4">
-        {campaigns.map((campaigns, idx) => (
+        {campaigns.map((campaign, idx) => (
           <Col key={idx}>
             <Card>
               <Card.Img
                 variant="top"
                 className="public-card-image"
-                src={`http://localhost:5000/uploads/${campaigns.image}`}
+                src={`http://localhost:5000/uploads/${campaign.image}`}
               />
               <Card.Body>
-                <Card.Title>{campaigns.name}</Card.Title>
-                <Card.Text>{campaigns.description}</Card.Text>
+                <Card.Title>{campaign.name}</Card.Title>
+                <Card.Text>{campaign.description}</Card.Text>
                 <Card.Text>
-                  Goal: LKR 
-                  {campaigns.target}
+                  Goal: LKR
+                  {campaign.target}
                 </Card.Text>
                 <Card.Text>
-                  Raised: LKR 
-                  {campaigns.raised}
+                  Raised: LKR
+                  {campaign.raised}
                 </Card.Text>
+
+                <CircularProgressbar
+                  value={(campaign.raised / campaign.target) * 100} // Calculate the percentage of goal achieved
+                  text={`${Math.round(
+                    (campaign.raised / campaign.target) * 100
+                  )}%`}
+                />
                 <Button
                   variant="primary"
                   className="primary_btn"
-                  onClick={() => handleDonateCampaignButton(campaigns.id)}
+                  onClick={() => handleDonateCampaignButton(campaign.id)}
                 >
                   Donate
                 </Button>
